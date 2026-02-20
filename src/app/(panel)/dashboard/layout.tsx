2f3e29bbe997df963b0d27ea681397b48747a1d4
hugodieb <hugodieb.hd@gmail.com>
2026-02-20 15:55:11 -0300

    links do sidebar

--- src/app/(panel)/dashboard/layout.tsx
+++ src/app/(panel)/dashboard/layout.tsx
@@ -2,25 +2,13 @@
 
 import React from "react";
 import Link from "next/link";
-import { Sidebar } from "../_components/sidebar";
+import { SidebarDashboard } from "./_components/sidebar";
 
 
 export default function DashboardLayout({ children }: { children: React.ReactNode }) {
-  return (
-    <div className="min-h-screen flex bg-background">
-      <aside className="w-64 border-r border-zinc-200 bg-white p-4">
-        <div className="mb-6">
-          <Link href="/dashboard" className="text-2xl font-bold text-zinc-900">
-            Odonto<span className="text-green-500">Pro</span>
-          </Link>
-        </div>
-
-        <Sidebar />
-      </aside>
-
-      <div className="flex-1">
-        {children}
-      </div>
-    </div>
-  );
+    return (
+        <SidebarDashboard>
+            {children}
+        </SidebarDashboard>
+    );
 }
