9ad7108c987b9c825804c5b388c43d704e33a9db
hugodieb <hugodieb.hd@gmail.com>
2026-02-23 17:44:39 -0300

    provider Github para login

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,9 +1,11 @@
 import NextAuth from "next-auth"
 import { PrismaAdapter } from "@auth/prisma-adapter"
 import prisma from "@/lib/prisma"
+import GitHub from "next-auth/providers/github"
+
  
 export const { handlers, auth, signIn, signOut } = NextAuth({
   adapter: PrismaAdapter(prisma),
   trustHost: true,
-  providers: [],
+  providers: [GitHub]
 })
