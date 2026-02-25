214cbc2361525f24b8b124ccd463440ebd3d921f
hugodieb <hugodieb.hd@gmail.com>
2026-02-25 12:43:55 -0300

    construção componente dialog com horarios da clinica

--- src/app/(panel)/dashboard/profile/_components/profile.tsx
+++ src/app/(panel)/dashboard/profile/_components/profile.tsx
@@ -1,5 +1,6 @@
 "use client"
 
+import { useState } from "react"
 import { useProfileForm } from "./profile-form-schema"
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card"
 import {
@@ -19,13 +20,60 @@ import {
   SelectTrigger,
   SelectValue
  } from "@/components/ui/select"
+
+import {
+  Dialog,
+  DialogClose,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog"
+
  import { Label } from "@/components/ui/label"
  import Image from "next/image"
  import imgTest from "../../../../../../public/doctor-hero.png"
+import { Button } from "@/components/ui/button"
+import { ArrowRight } from "lucide-react"
+import { cn } from "@/lib/utils"
 
 export function ProfileContent() {
 
+  const [selectHours, setSelectHours] = useState<string[]>([])
+
+  const [openDialog, setOpenDialog] = useState(false)
+
   const form = useProfileForm()
+
+  function generateTimeSlots(): string[] {
+    const timeSlots: string[] = [];
+    const startTime = new Date();
+    startTime.setHours(8, 0, 0, 0); // Início às 8:00
+
+    const endTime = new Date();
+    endTime.setHours(24, 0, 0, 0); // Fim às 18:00
+
+    while (startTime < endTime) {
+      const hours = startTime.getHours().toString().padStart(2, '0');
+      const minutes = startTime.getMinutes().toString().padStart(2, '0');
+      timeSlots.push(`${hours}:${minutes}`);
+      startTime.setMinutes(startTime.getMinutes() + 30); // Incrementa em 30 minutos
+    }
+
+    return timeSlots;
+  }
+
+  const timeSlots = generateTimeSlots()
+
+  function toggHour(hour: string) {
+    if (selectHours.includes(hour)) {
+      setSelectHours(selectHours.filter((h) => h !== hour));
+    } else {
+      setSelectHours([...selectHours, hour]);
+    }
+  }
 
   return (
 
@@ -127,6 +175,43 @@ export function ProfileContent() {
                     </FormItem>
                   )}
                 />
+
+                <div>
+                  <Label id="Horario" className="font-semibold">Configurar horários da clínica</Label>
+                  <Dialog open={openDialog} onOpenChange={setOpenDialog}>
+                    <DialogTrigger asChild className="w-full">
+                      <Button variant="outline" className="w-full mt-2 justify-between text-taupe-800">
+                        Clique aqui para selecionar horários
+                        <ArrowRight className="w-5 h-5" />
+                      </Button>                      
+                    </DialogTrigger>
+                    <DialogContent>
+                      <DialogHeader>
+                        <DialogTitle>Fuso Horário</DialogTitle>
+                        <DialogDescription>
+                          Selecione os horários de funcionamento da clínica.
+                        </DialogDescription>
+                        
+                        <div className="grid grid-cols-4 gap-2 my-4">
+                          {timeSlots.map((time) => (
+                            <Button
+                              key={time}
+                              onClick={() => toggHour(time)}
+                              variant="outline"
+                              size="sm"
+                              className={cn('h-10', selectHours.includes(time) && 'border-2 border-emerald-500 text-primary')}
+                            >
+                              {time}
+                            </Button>
+                          ))}
+                        </div>
+                        <Button onClick={() => setOpenDialog(false)} className="mt-4 w-full">
+                          Ao fechar essa janela, salve os dados!
+                        </Button>
+                      </DialogHeader>
+                    </DialogContent>
+                  </Dialog>
+                </div>
               </div>                            
             </CardContent>
           </Card>
